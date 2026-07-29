dde1bbd8fbf8a170dbdd21a4c34ef29c8c525f05
funny-nguyen <hoan-nguyen@gmx.de>
2026-07-29 23:08:21 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+console.log("CodeCards V3 gestartet");
+
+
+
+const startButton =
+document.getElementById("startButton");
+
+
+
+if (startButton) {
+
+
+    startButton.addEventListener(
+        "click",
+        () => {
+
+
+            alert(
+                "Willkommen bei CodeCards V3! 🚀"
+            );
+
+
+        }
+    );
+
+
+}
