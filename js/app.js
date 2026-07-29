c65856bb5ece265c57d20c83f3966c4e199b9a0b
funny-nguyen <hoan-nguyen@gmx.de>
2026-07-29 23:14:17 +0200

    Add first flashcard learning logic

--- js/app.js
+++ js/app.js
@@ -1,27 +1,180 @@
-console.log("CodeCards V3 gestartet");
+console.log("CodeCards V3 Lernmodus gestartet");
 
 
 
-const startButton =
-document.getElementById("startButton");
+let cards = [];
+
+let currentCardIndex = 0;
 
 
 
-if (startButton) {
+const category =
+document.getElementById("category");
 
 
-    startButton.addEventListener(
+const question =
+document.getElementById("question");
+
+
+const answer =
+document.getElementById("answer");
+
+
+const showAnswer =
+document.getElementById("showAnswer");
+
+
+const rating =
+document.getElementById("rating");
+
+
+
+
+
+async function loadCards() {
+
+
+    const response =
+    await fetch("data/cards.json");
+
+
+    cards =
+    await response.json();
+
+
+
+    showCard();
+
+}
+
+
+
+
+
+function showCard() {
+
+
+    const card =
+    cards[currentCardIndex];
+
+
+
+    category.textContent =
+    card.category;
+
+
+
+    question.textContent =
+    card.question;
+
+
+
+    answer.textContent =
+    card.answer;
+
+
+
+    answer.style.display =
+    "none";
+
+
+
+    rating.style.display =
+    "none";
+
+
+
+    showAnswer.style.display =
+    "inline-block";
+
+}
+
+
+
+
+
+showAnswer.addEventListener(
+    "click",
+    () => {
+
+
+        answer.style.display =
+        "block";
+
+
+        rating.style.display =
+        "block";
+
+
+        showAnswer.style.display =
+        "none";
+
+
+    }
+);
+
+
+
+
+
+document
+.querySelectorAll("#rating button")
+.forEach(button => {
+
+
+    button.addEventListener(
         "click",
         () => {
 
 
-            alert(
-                "Willkommen bei CodeCards V3! 🚀"
+            const result =
+            button.dataset.rating;
+
+
+
+            console.log(
+                "Bewertung:",
+                result
             );
+
+
+
+            nextCard();
 
 
         }
     );
 
 
+});
+
+
+
+
+
+function nextCard() {
+
+
+    currentCardIndex++;
+
+
+
+    if(
+        currentCardIndex >= cards.length
+    ){
+
+        currentCardIndex = 0;
+
+    }
+
+
+
+    showCard();
+
 }
+
+
+
+
+
+loadCards();
